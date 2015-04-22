abd38d832359da38355d061d9ce6743e9de57cab
Geir Sagberg <geir.sagberg@gmail.com>
2015-04-22 15:45:06 +0000

    Lagt til UsersController.

---
+++ LogStore.DashBoard/src/LogStore.DashBoard.Web/Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace LogStore.DashBoard.Web.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
